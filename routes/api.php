804b06f77da649b255701aff7e02bac6767f3dfe
alifa <alifa.qadri@gmail.com>
2025-12-11 16:14:16 +0700

    memberikan response json untuk notifikasi midtrans

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/notification', function (Request $request) {
-    \Log::info('Midtrans Notification:', $request->all());
-    return response()->json(['message' => 'ok']);
+    $payload = $request->all();
+    return response()->json([
+        'message' => 'Notification received successfully',
+        'payload' => $payload
+    ], 200);
 });
